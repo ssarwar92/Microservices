99cadc13876cc6ca1b728c1c6ba1692f64c7993b
Sarwar <gssarwar@gmail.com>
2018-10-09 23:27:31 +0530

    limits-service

---
+++ limits-service/src/main/java/com/gs/microservices/limitsservice/LimitConfiguration/LimitsConfigurationController.java
@@ -0,0 +1,23 @@
+package com.gs.microservices.limitsservice.LimitConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gs.microservices.limitsservice.Configuration;
+import com.gs.microservices.limitsservice.bean.LimitConfiguration;
+
+
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+
+}
